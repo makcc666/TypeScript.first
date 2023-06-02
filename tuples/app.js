b8df7a1353269f4b08a3c1afcbf554b6312ce77a
makcc666 <makcc6666@gmail.com>
2023-06-02 21:36:11 +0300

    Generic + ReadOnly

--- tuples/app.js
+++ tuples/app.js
@@ -7,3 +7,22 @@ const arr = [1, "some_string", false, true, false,];
     const arr_second = [1, "some_string", false, true, false,];
     // arr_first === arr_second
 }
+// ReadOnly
+{
+    {
+        const skill = [1, "Dev"];
+    }
+    // Eq, generic
+    {
+        const skill__like_generic = ["First", "Second"];
+        const skill__not_like_generic = ["First", "Second"];
+        const skill__like_generic__readonly = ["First", "Second"];
+    }
+    // Не работает, можно перезаписывать
+    {
+        const arr = [1, "some_string", false, true, false,];
+        arr[0] = 1;
+        arr[2] = false;
+        console.log("arr::", arr);
+    }
+}
--- tuples/app.ts
+++ tuples/app.ts
@@ -10,3 +10,32 @@ const arr: [number, string, ...boolean[]] = [1, "some_string", false, true, fals
     const arr_second: [number, string, ...[boolean, boolean, boolean]] = [1, "some_string", false, true, false,];
     // arr_first === arr_second
 }
+
+
+// ReadOnly
+{
+    {
+        const skill: readonly [number, string] = [1, "Dev"];
+
+    }
+
+    // Eq, generic
+    {
+        const skill__like_generic: Array<string> = ["First", "Second"];
+        const skill__not_like_generic: string[] = ["First", "Second"];
+        const skill__like_generic__readonly: ReadonlyArray<string> = ["First", "Second"];
+
+    }
+
+
+    // Не работает, можно перезаписывать
+    {
+        const arr: [number, string, ...readonly boolean[]] = [1, "some_string", false, true, false,];
+        arr[0] = 1;
+        arr[2] = false;
+
+        console.log("arr::", arr);
+    }
+
+}
+
