899f7c1447a6911140e096a5e00a10d9d323bcb3
makcc666 <makcc6666@gmail.com>
2023-06-06 13:31:26 +0300

    Type Guard.

---
+++ Part_2/practice_type_guard/app.ts
@@ -0,0 +1,83 @@
+interface IPayment {
+    sum: number;
+    from: number;
+    to: number;
+}
+
+enum PaymentStatus {
+    Success = 'success',
+    Failed = 'failed',
+}
+
+interface IPaymentRequest extends IPayment {
+}
+
+interface IDataSuccess extends IPayment {
+    databaseId: number;
+}
+
+interface IDataFailed {
+    errorMessage: string;
+    errorCode: number;
+}
+
+interface IResponseSuccess {
+    status: PaymentStatus.Success;
+    data: IDataSuccess;
+}
+
+interface IResponseFailed {
+    status: PaymentStatus.Failed;
+    data: IDataFailed;
+}
+
+type f = (res: IResponseSuccess) => number;
+
+const is_pay_response_success = (res: IResponseSuccess | IResponseFailed): res is IResponseSuccess => {
+    return res?.status === PaymentStatus.Success;
+}
+const is_pay_response_fail = (res: IResponseSuccess | IResponseFailed): res is IResponseFailed => {
+    return res?.status === PaymentStatus.Failed;
+}
+
+type Res = IResponseSuccess | IResponseFailed;
+
+const get_payment_database_id = (res:Res):number=>{
+    if (is_pay_response_success(res)){
+        return res.data.databaseId;
+    }
+    throw new Error(res.data.errorMessage);
+}
+
+/*
+const send_payment = async (req: IPayment): Promise<number> => {
+    const res = await fetch("/api/payment", {
+        method: "post",
+        body: JSON.stringify(req),
+    });
+    const json_data:Res = await res.json();
+    if (is_pay_response_success(json_data)) {
+        return json_data.data.databaseId;
+    }
+
+    throw json_data;
+}
+
+(async () => {
+    try {
+        const data = await send_payment({
+            sum: 5,
+            from: 10,
+            to: 111,
+        });
+        console.log("pay database_id is::", data);
+    } catch (e) {
+        if (is_pay_response_fail(e)) {
+            console.error(e.data.errorMessage);
+        } else {
+            console.error("Unknown error");
+        }
+    }
+})();
+
+ */
