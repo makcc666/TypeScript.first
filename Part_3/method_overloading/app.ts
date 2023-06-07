d3e04c0427526a5e79798af3f4a75427a5fe8f3f
makcc666 <makcc6666@gmail.com>
2023-06-08 00:34:08 +0300

    Getter & Setter

--- Part_3/method_overloading/app.ts
+++ Part_3/method_overloading/app.ts
@@ -1,43 +1,17 @@
-{
-    type Skill = string;
-    // Принимает только не пустой массив из Skill
-    type Skills = [Skill, ...Skill[]];
+class User {
+    private _login: string;
+    private _password: string;
 
-
-    class User {
-        skills: Skill[] = [];
-
-        addSkills(skill: string): void;
-        addSkills(skills: Skills): void;
-        addSkills(skills: Skills | Skill) {
-            if (typeof skills === "string") {
-                this.skills.push(skills);
-            } else {
-                this.skills.push(...skills);
-            }
-
-        }
+    set login(value: string) {
+        this._login = `user-${value}`;
     }
 
-    const user = new User();
-    user.addSkills(["PC"]);
-    console.log("user::skills::0", user.skills);
-
-    user.addSkills(["Head", "JS"]);
-    console.log("user::skills::1", user.skills);
-
-    user.addSkills("NodeJS");
-    console.log("user::skills::2", user.skills);
-}
-
-{
-    function run(distance: string): string;
-    function run(distance: number): number;
-    function run(distance: string | number): string | number {
-        if (typeof distance === "string") return `is string::${distance}`;
-        return distance + 1;
+    get login() {
+        return this._login;
     }
 
-    run(1);
-    run("asfsaf");
 }
+
+const user = new User();
+user.login = "Ursa";
+console.log("user.login::",user.login);
