2898806db79d6d8127acd8818552805de6b271af
makcc666 <makcc6666@gmail.com>
2023-06-22 14:41:51 +0300

    Fix namespace

--- Part_8 Decorators/pattern/app.ts
+++ Part_8 Decorators/pattern/app.ts
@@ -1,30 +1,32 @@
-interface IUserService {
-	users: number;
+{
+	interface IUserService {
+		users: number;
 
-	getUsersInDatabase(): number;
-}
-
-
-class UserService implements IUserService {
-	users: number=520;
-
-	getUsersInDatabase(): number {
-		return this.users;
+		getUsersInDatabase(): number;
 	}
 
-}
 
-function nullUser(obj: UserService):UserService {
-	obj.users = 0;
-	return obj;
-}
+	class UserService implements IUserService {
+		users: number=520;
 
-function logUsers(obj: UserService):UserService {
-	console.log("logUsers::users count::", obj.users);
-	return obj;
-}
+		getUsersInDatabase(): number {
+			return this.users;
+		}
 
-console.log(new UserService().getUsersInDatabase());
-console.log(nullUser(new UserService()).getUsersInDatabase());
-console.log(logUsers(nullUser(new UserService())).getUsersInDatabase());
-console.log(nullUser(logUsers(new UserService())).getUsersInDatabase());
+	}
+
+	function nullUser(obj: UserService):UserService {
+		obj.users = 0;
+		return obj;
+	}
+
+	function logUsers(obj: UserService):UserService {
+		console.log("logUsers::users count::", obj.users);
+		return obj;
+	}
+
+	console.log(new UserService().getUsersInDatabase());
+	console.log(nullUser(new UserService()).getUsersInDatabase());
+	console.log(logUsers(nullUser(new UserService())).getUsersInDatabase());
+	console.log(nullUser(logUsers(new UserService())).getUsersInDatabase());
+}
